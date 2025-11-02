153faf54fd91fdb16308a482d4b24aa26e94d0ca
Divya Sundhari S M <220701068@rajalakshmi.edu.in>
2025-11-02 17:01:41 +0530

    Change redirect URI in msalConfig
    Updated redirect URI for Azure AD authentication.

--- src/msalconfig.js
+++ src/msalconfig.js
@@ -2,7 +2,7 @@ export const msalConfig = {
 auth: {
 clientId: "93e9d75b-8c48-4930-a7d1-36e6b35b41a7", // replace with Azure AD app (Application) (client) ID
 authority: "https://login.microsoftonline.com/834c2bbe-3a4a-4347-991a-5339c5222571", // replace with your tenant ID or common
-redirectUri: "http://localhost:5173"
+redirectUri: "https://leavedeskweb-bjc6gmg7hmcpcec9.centralindia-01.azurewebsites.net"
 },
 cache: {
 cacheLocation: "localStorage",
